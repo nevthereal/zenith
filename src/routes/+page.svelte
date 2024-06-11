1366cac7d1c9b16574abc865b91c235ada3437d2
Neville Brem <bremneville@gmail.com>
2024-06-11 18:26:56 +0200

    made homepage prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-	import dayjs from 'dayjs';
 	import type { PageServerData } from './$types';
 	import { superForm } from 'sveltekit-superforms';
 
 	export let data: PageServerData;
 
-	const { form } = superForm(data.form);
+	const { form, delayed, enhance, message } = superForm(data.form);
 </script>
 
 <div class="flex flex-col items-center">
 	<h1 class="mb-16 text-5xl font-bold">What are you up to?</h1>
-	<form method="POST" class="flex items-center gap-4">
+	<form method="POST" class="flex items-center gap-4" use:enhance>
 		<input
 			class="input input-bordered input-primary border-2"
 			type="text"
@@ -18,6 +17,14 @@
 			name="task"
 			bind:value={$form.task}
 		/>
-		<button class="btn btn-primary">Add!</button>
+		<button class="btn btn-primary"
+			>Add!
+			{#if $delayed}
+				<span class="loading loading-spinner loading-xs"></span>
+			{/if}
+		</button>
 	</form>
+	{#if $message}
+		<p>{$message}</p>
+	{/if}
 </div>
