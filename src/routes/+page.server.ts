9d6aa91f91c7875e60b8454f6cb66d3951015d6b
Neville Brem <bremneville@gmail.com>
2024-06-11 15:19:07 +0200

    added form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,27 @@
 import { taskSchema } from '$lib';
 import { model } from '$lib/ai';
 import { generateObject } from 'ai';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+const formSchema = z.object({
+	task: z.string()
+});
 
 export const load: PageServerLoad = async () => {
-	const { object } = await generateObject({
-		model: model,
-		schema: taskSchema,
-		prompt: 'Today is the 11 of June 2024. Dinner tomorrow at 9PM'
-	});
+	const form = await superValidate(zod(formSchema));
 
-	return { object };
+	return { form };
+};
+
+export const actions: Actions = {
+	default: async () => {
+		const { object } = await generateObject({
+			model: model,
+			schema: taskSchema,
+			prompt: 'Today is the 11 of June 2024. Dinner tomorrow at 9PM'
+		});
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
 	import type { PageServerData } from './$types';
+	import { superForm } from 'sveltekit-superforms';
 
 	export let data: PageServerData;
 
-	const task = data.object;
-	const due = dayjs(task.due);
+	const { form } = superForm(data.form);
 </script>
 
+<form method="POST" class="flex w-[50vw] flex-col">
+	<label for="task">Task:</label>
+	<input class="rounded-2xl border-2 p-2" type="text" name="task" bind:value={$form.task} />
+</form>
+
 <div class="rounded-2xl border-2 p-4">
-	<h1 class="mb-2">{task.title}</h1>
-	<p>{due.toDate().toLocaleString([], { dateStyle: 'medium', timeStyle: 'short' })}</p>
+	<h1 class="mb-2">Task name</h1>
+	<p>Date</p>
 </div>
