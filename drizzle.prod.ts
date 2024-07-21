9c5206584f854867e3120160b10aa1ce468ff070
Neville Brem <bremneville@gmail.com>
2024-07-21 12:53:31 +0200

    added a dev script for drizzle

---
+++ drizzle.prod.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
+
+export default {
+	dbCredentials: {
+		url: process.env.DB_URL as string
+	},
+	dialect: 'postgresql',
+	schema: './src/lib/db/schema.ts',
+	out: './drizzle'
+} satisfies Config;
