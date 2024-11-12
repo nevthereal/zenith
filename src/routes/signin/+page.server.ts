a8b7c3efb652e39838147ca0aef68521e70d136a
Neville Brem <bremneville@gmail.com>
2024-11-12 07:29:17 +0100

    changed some prerendering stuff

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,27 +1,10 @@
-import { generateState } from 'arctic';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
-import { github } from '$lib/auth';
+
+export const prerender = true;
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.user) redirect(302, '/account');
 
 	return { user: locals.user };
-};
-
-export const actions: Actions = {
-	github: async ({ cookies }) => {
-		const state = generateState();
-		const url = await github.createAuthorizationURL(state);
-
-		cookies.set('github_oauth_state', state, {
-			path: '/',
-			secure: import.meta.env.PROD,
-			httpOnly: true,
-			maxAge: 60 * 10,
-			sameSite: 'lax'
-		});
-
-		redirect(302, url.toString());
-	}
 };
