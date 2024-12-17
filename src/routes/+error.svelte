25badb2016091a96efb4e48269f7e7df5fd9244e
Neville Brem <bremneville@gmail.com>
2024-12-17 06:46:38 +0100

    updated sk

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <div class="text-center">
 	<h1 class="heading-main mb-4 mt-8">Something went wrong</h1>
 	<p class="my-4 text-lg font-medium text-error">
-		{$page.status}: {$page.error?.message}
-		{#if $page.error}
+		{page.status}: {page.error?.message}
+		{#if page.error}
 			<i class="fa-solid fa-triangle-exclamation"></i>
 		{/if}
 	</p>
