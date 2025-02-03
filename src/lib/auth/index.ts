4453b817f4e86854568fb211efdc5cda752c7e1c
Neville Brem <contact@nevillebrem.com>
2025-02-03 20:53:16 +0100

    dont need that ig

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,12 +1,9 @@
-import { dev } from '$app/environment';
 import { GITHUB_CLIENT_ID, GITHUB_SECRET } from '$env/static/private';
-import { PUBLIC_BASE_URL } from '$env/static/public';
 import { db } from '../db';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
 export const auth = betterAuth({
-	baseURL: dev ? PUBLIC_BASE_URL : process.env.VERCEL_URL,
 	emailAndPassword: {
 		enabled: false
 	},
