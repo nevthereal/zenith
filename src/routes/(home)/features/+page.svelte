c308a913d26be8bbce3693d041207d484eb64a8d
Neville Brem <contact@nevillebrem.com>
2025-03-08 21:02:22 +0100

    update features page

--- src/routes/(home)/features/+page.svelte
+++ src/routes/(home)/features/+page.svelte
@@ -19,7 +19,9 @@
 		Why? Because I think clicking through menus after menus is very tedious and has a lot of
 		friction. Just having one text field is much more convenient and with the understanding
 		capabilities of <strong>GPT-4o</strong> the task or event will be generated. To not skyrocket my
-		OpenAI bill, I have set the amount of requests to 10 per hour.
+		OpenAI bill, I have set the amount of requests to 10 per hour. If you are on the
+		<strong>free tier</strong>, you are restricted to
+		<strong>5 event generations per day</strong>.
 	</p>
 	<p>
 		Right now, this is the only AI-powered feature, but I am planning to add recommendations for
@@ -29,15 +31,16 @@
 	<p>
 		In Zenith, you have the ability to organize your events into projects. In a future iteration you
 		will be able to share them with other users. Projects consist of a title and an optional
-		deadline.
+		deadline. In the <strong>free tier</strong>, you are restricted to
+		<strong>one project per user</strong>.
 	</p>
 	<h2 id="pricing">Pricing</h2>
 	<p>
 		Since using AI and storing Data somewhere isn't free you have to purchase this product for <strong
 			>$5/month</strong
 		>
-		to use all of it's features. This way you will have access to all of the features. Your payment is
-		securely processed with
+		to create (nearly) unlimited events per day and as many projects as you want. This way you will have
+		access to all of the features. Your payment is securely processed with
 		<strong>Stripe</strong>.
 	</p>
 	<p>
